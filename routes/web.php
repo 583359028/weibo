5fb1dc7da14396300b9123011d76dc4fe8559d01
dream <583359028@qq.com>
2019-06-13 18:35:06 +0000

    用户显示界面

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,17 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 route::get('signup','UsersController@create')->name('signup');
 
+route::resource('users','UsersController');
+
+/* 相当于 route::resource('users','UsersController');
+Route::get('/users', 'UsersController@index')->name('users.index');显示所有用户的界面
+Route::get('/users/create', 'UsersController@create')->name('users.create'); 创建用户的界面
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');显示用户个人信息的界面
+Route::post('/users', 'UsersController@store')->name('users.store'); 创建用户
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit'); 编辑用户个人资料的界面
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');更新用户
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy'); 删除用户  */
+
 
 /*Route::get('/', function () {
     return view('welcome');
